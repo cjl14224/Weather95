999cebd7672ae3dc2ab9e26cbf52231f0814d463
Cody Lange <cjl14224@gmail.com>
2022-12-18 18:01:02 -1000

    added navbar

--- weather95/src/App.js
+++ weather95/src/App.js
@@ -1,21 +1,45 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import { MenuList, MenuListItem, Separator, styleReset, Button } from "react95";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+/* Pick a theme of your choice */
+import original from "react95/dist/themes/original";
+
+/* Original Windows95 font (optional) */
+import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
+import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
+
+import Navbar from "./components/Navbar";
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body, input, select, textarea {
+    font-family: 'ms_sans_serif';
+  }
+  body{
+    background-color: #018181;
+  }
+  ${styleReset}
+`;
+
+const App = () => (
+  <div>
+    <GlobalStyles />
+    <ThemeProvider theme={original}>
+      <Navbar />
+    </ThemeProvider>
+  </div>
+);
 
 export default App;
